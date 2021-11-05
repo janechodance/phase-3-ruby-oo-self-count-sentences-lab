e132f9a17ab7cf769ee44fa50d1bb90a61cf70b3
Jane <janechodance@gmail.com>
2021-11-05 10:02:20 -0400

    commit tree 4d88c26f528e83fcc1648bca66182a7e38476915

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,33 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
+    number = 0
+     count = self.split
+     count.each  do |a|
+      if a.sentence?
+        number +=1
+      elsif
+        a.question?
+        number +=1
+      elsif a.exclamation?
+        number +=1
+      else
+        number = number
+      end
+    end
+      number
 
   end
 end
